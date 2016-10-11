d6ef775bc3bd8e62c96cd64746b778764e533422
Curious Kali <kalicurious@gmail.com>
2016-10-12 00:12:10 +0200

    init -- control_panel pkg -- help

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,5 +7,9 @@
 # new environment variables
 
 # modified environment variables
-export PKG_CONFIG_PATH="/opt/ros/indigo/lib/pkgconfig"
+export CMAKE_PREFIX_PATH="/home/parth/alpheus/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/parth/alpheus/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/parth/alpheus/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/parth/alpheus/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/parth/alpheus/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/parth/alpheus/src:$ROS_PACKAGE_PATH"
